c2f0ec311833dbbbbc3f312274f9d675f133145d
Pseudoioannes <pseudoioannes@gmail.com>
2022-06-20 10:50:45 +0600

    73

--- meadowlark.js
+++ meadowlark.js
@@ -4,15 +4,19 @@ const app = express();
 
 app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3000;
+
+const fortunes = ["conquer your fears", "Do not fear", "Keep it simple"];
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  const randomfortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomfortune });
 });
 app.use((req, res) => {
   res.status(404);
