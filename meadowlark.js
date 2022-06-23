7a8a5f47b52d7f305c9dd3dc9fc5869ee6db6797
Pseudoioannes <pseudoioannes@gmail.com>
2022-06-23 13:23:49 +0600

    page 103

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,9 @@ const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const app = express();
 
-const fortune = require("./lib/fortune");
+const handlers = require("./lib/handlers");
+
+// const fortune = require("./lib/fortune");
 
 app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -10,24 +12,12 @@ app.use(express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.get("/", handlers.home);
 
-app.get("/about", (req, res) => {
-  res.render("about", { fortune: fortune.getFortune() });
-});
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.get("/about", handlers.about);
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-  console.error(err.message);
-
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => {
   console.log(`Express running server on ${port}`);
